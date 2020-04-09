be0f2efb9951859a02efb16e4b83810d55d6a21c
Bill <592383484@qq.com>
2020-04-09 22:48:49 +0800

    Statis function for reading and writing text files as

---
+++ Exercises/src/P18/TextFile.java
@@ -0,0 +1,44 @@
+//a single string,and treating a file as an Arraylist.
+package P18;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TextFile extends ArrayList{
+	//Read a file as a single string;
+	public static String read(String fileName) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		try{
+			BufferedReader in = new BufferedReader(new FileReader(
+					new File(fileName).getAbsoluteFile()));
+			try{
+				String s;
+				while((s=in.readLine())!=null){
+					sb.append(s);
+					sb.append("\n");
+				}
+			}finally{
+				in.close();
+			}
+		}catch(IOException e){
+			throw e;
+		}
+		return sb.toString();
+	}
+	
+	public TextFile(String name,String spliter) throws IOException{
+		super(Arrays.asList(read(name).split(spliter)));
+		if(get(0).equals("")) remove(0);
+	}
+	
+	public TextFile(String name) throws IOException{
+		this(name,"\n");
+	}
+
+	public static void main(String[] args){
+		
+	}
+}
