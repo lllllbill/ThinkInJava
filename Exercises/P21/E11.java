5a43e7348146d9d325a8f1a65a26a652808d7d98
Bill <592383484@qq.com>
2020-04-29 23:08:02 +0800

    Exercise P21 E11

---
+++ Exercises/P21/E11.java
@@ -0,0 +1,43 @@
+package P21;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+
+public class E11 implements Runnable{
+	private String[] music = "我和你吻别，在无人的街道,一起呼叫，没有烦恼，烦恼什么烦恼~".split("");
+	private volatile int data2;
+	
+	public void unstabitilyOut(){
+		System.out.print(music[data2]);
+		Thread.yield();
+		++data2;
+	}
+	
+	public synchronized void synchronizedOut(){
+		System.out.print(music[data2]);
+		Thread.yield();
+		++data2;
+	}
+	
+	@Override
+	public void run() {
+		while(data2<music.length){
+			unstabitilyOut();
+			//synchronizedOut();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newFixedThreadPool(10);
+		E11 e11 =new E11();
+		for(int i=0;i<10;i++){
+			exec.execute(e11);
+		}
+	}
+
+}
+
+	
